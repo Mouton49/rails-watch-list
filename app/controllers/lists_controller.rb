64ee544f8117d0e370972f37b14487d41cc115e0
abdehollain@gmail.com <abdehollain@gmail.com>
2022-05-20 17:08:30 +0200

    done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,7 +14,7 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to list_path(@list)
+      redirect_to root_path
     else
       render :new
     end
@@ -22,6 +22,6 @@ class ListsController < ApplicationController
 
   private
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
